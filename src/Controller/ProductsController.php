6156b4be08fd8964086ed90a618241b3e95e4da4
sahil <sahilvernekar96@gmail.com>
2024-10-26 15:37:38 +0530

    set up symfony server on local

---
+++ src/Controller/ProductsController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductsController
+{
+
+    #[Route('/products/{id}/lowest-price',name:'lowest-price',methods:'POST')]
+    public function lowestPrice(int $id): Response
+    {
+        dd($id);
+    }
+}
