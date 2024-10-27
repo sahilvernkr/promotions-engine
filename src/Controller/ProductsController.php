02160ad1a55c3bed95f8f89588244cbe4f37b92d
sahil <sahilvernekar96@gmail.com>
2024-10-27 16:31:27 +0530

    false response

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,15 +2,32 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController
 {
 
-    #[Route('/products/{id}/lowest-price',name:'lowest-price',methods:'POST')]
-    public function lowestPrice(int $id): Response
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: 'POST')]
+    public function lowestPrice(Request $request, int $id): Response
     {
-        dd($id);
+        if ($request->headers->has('force_fail')) {
+            return new JsonResponse([
+                'error' => 'Promotions Engine failure message'
+            ], $request->headers->get('force_fail'));
+        }
+
+        return new JsonResponse([
+            'quantity' => 5,
+            'request_location' => 'UK',
+            'voucher_code' => 'OU812',
+            'request_date' => $id,
+            'price' => 100,
+            'discounted_proce' => 50,
+            'promotion_id' => 3,
+            'promotion_name' => 'Black Friday Sale'
+        ], 200);
     }
 }
