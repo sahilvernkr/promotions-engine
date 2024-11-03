500e2b96ad32e3faaf910c156b7e923982d47423
sahil <sahilvernekar96@gmail.com>
2024-11-03 13:28:11 +0530

    PriceModifer test

--- tests/Unit/PriceModifiersTest.php
+++ tests/Unit/PriceModifiersTest.php
@@ -9,7 +9,7 @@ use App\Filter\Modifier\DateRangeMultiplier;
 
 class PriceModifiersTest extends ServiceTestCase
 {
-    /**@test */
+    /** @test */
     public function DateRangeMultiplier_returns_a_correctly_modified_price(): void
     {
         //Given
@@ -24,15 +24,38 @@ class PriceModifiersTest extends ServiceTestCase
         $promotion->setCriteria(["from" => "2022-11-25", "to" => "2022-11-28"]);
         $promotion->setType('date_range_multiplier');
 
-
         $dateRangeModifier = new DateRangeMultiplier();
-
 
         //When
         $modifiedPrice = $dateRangeModifier->modify(100, 5, $promotion, $enquiry);
 
-
         //Then
         $this->assertEquals(250, $modifiedPrice);
     }
+
+    //  /** @test */
+    //  public function FixedPriceVoucher_returns_a_correctly_modified_price(): void
+    //  {
+    //      //Given
+ 
+    //      $enquiry = new LowestPriceEnquiry();
+    //      $enquiry->setQuantity(5);
+    //      $enquiry->setRequestDate('2022-11-27');
+ 
+    //      $promotion = new Promotion();
+    //      $promotion->setName('Black Friday half price sale');
+    //      $promotion->setAdjustment(0.5);
+    //      $promotion->setCriteria(["from" => "2022-11-25", "to" => "2022-11-28"]);
+    //      $promotion->setType('date_range_multiplier');
+ 
+    //      $dateRangeModifier = new DateRangeMultiplier();
+ 
+    //      //When
+    //      $modifiedPrice = $dateRangeModifier->modify(100, 5, $promotion, $enquiry);
+ 
+    //      //Then
+    //      $this->assertEquals(250, $modifiedPrice);
+    //  }
+
+
 }
