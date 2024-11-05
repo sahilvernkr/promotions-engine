65fb5774a94e9ffed7c6f31d2ad14601fa91343d
sahil <sahilvernekar96@gmail.com>
2024-11-05 22:16:02 +0530

    blank commit

--- src/Filter/LowestPriceFilter.php
+++ src/Filter/LowestPriceFilter.php
@@ -7,27 +7,23 @@ use App\DTO\PromotionEnquiryInterface;
 use App\Filter\Modifier\Factory\PriceModifierFactoryInterface;
 
 class LowestPriceFilter implements PromotionsFilterInterface
-{ 
-  public function __construct(private PriceModifierFactoryInterface  $priceModifierFactory)
-  {
-    
-  }
+{
+  public function __construct(private PriceModifierFactoryInterface  $priceModifierFactory) {}
   public function apply(PromotionEnquiryInterface $enquiry, Promotion ...$promotions): PromotionEnquiryInterface
   {
     $price = $enquiry->getProduct()->getPrice();
     $quantity = $enquiry->getQuantity();
     $lowestPrice = $quantity * $price;
 
-    foreach($promotions as $promotion){
-    $priceModifier = $this->priceModifierFactory->create($promotion->getType());
+    foreach ($promotions as $promotion) {
+      $priceModifier = $this->priceModifierFactory->create($promotion->getType());
 
-    $modifiedPrice = $priceModifier->modify($price, $quantity, $promotion, $enquiry);
+      $modifiedPrice = $priceModifier->modify($price, $quantity, $promotion, $enquiry);
 
-
-    $enquiry->setDiscountedPrice(250);
-    $enquiry->setPrice(100);
-    $enquiry->setPromotionId(3);
-    $enquiry->setPromotionName('Black Friday Sale');
+      $enquiry->setDiscountedPrice(250);
+      $enquiry->setPrice(100);
+      $enquiry->setPromotionId(3);
+      $enquiry->setPromotionName('Black Friday Sale');
     }
     return $enquiry;
   }
