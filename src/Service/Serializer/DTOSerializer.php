866870c3070c73139ad55cdc16d56a03085cd847
sahil <sahilvernekar96@gmail.com>
2024-11-12 08:52:41 +0530

    create event

--- src/Service/Serializer/DTOSerializer.php
+++ src/Service/Serializer/DTOSerializer.php
@@ -32,6 +32,10 @@ class DTOSerializer implements SerializerInterface
 
     public function deserialize(mixed $data, string $type, string $format, array $context = []): mixed
     {
-        return $this->serializer->deserialize($data, $type, $format, $context);
+        $dto =  $this->serializer->deserialize($data, $type, $format, $context);
+        
+        //fire an event here
+
+        return $dto;
     }
 }
