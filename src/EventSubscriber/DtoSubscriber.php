e402aa4dda8cc1762bdd191144b8b649f06f38a0
sahil <sahilvernekar96@gmail.com>
2024-11-12 09:01:30 +0530

    create eventSubscriber

---
+++ src/EventSubscriber/DtoSubscriber.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Event\AfterDtoCreatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class DtoSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents()
+    {
+        return [
+            AfterDtoCreatedEvent::NAME => 'validateDto'
+        ];
+    }
+
+    public function validateDto(AfterDtoCreatedEvent $event): void
+    {
+        //TODO: validate the dto
+    }
+}
