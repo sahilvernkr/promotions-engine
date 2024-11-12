259a47077010881561471335ad4b0fcb44466382
sahil <sahilvernekar96@gmail.com>
2024-11-12 09:14:29 +0530

    add DTO validation

--- src/EventSubscriber/DtoSubscriber.php
+++ src/EventSubscriber/DtoSubscriber.php
@@ -4,9 +4,13 @@ namespace App\EventSubscriber;
 
 use App\Event\AfterDtoCreatedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class DtoSubscriber implements EventSubscriberInterface
 {
+    public function __construct(private ValidatorInterface $validator) {}
+    
     public static function getSubscribedEvents()
     {
         return [
@@ -16,6 +20,12 @@ class DtoSubscriber implements EventSubscriberInterface
 
     public function validateDto(AfterDtoCreatedEvent $event): void
     {
-        //TODO: validate the dto
+        $dto = $event->getDto();
+
+        $errors = $this->validator->validate($dto);
+
+        if(count($errors) > 0){
+            throw new ValidationFailedException('validation failed', $errors);
+        }
     }
 }
---
+++ tests/Unit/DtoSubscriberTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit;
+
+use App\Tests\ServiceTestCase;
+
+class DtoSubscriberTest extends ServiceTestCase
+{
+    /** @test */
+    public function a_dto_is_validated_after_it_has_been_created(){
+        
+    }
+}
